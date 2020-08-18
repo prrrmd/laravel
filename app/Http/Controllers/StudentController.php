15e9f9da1f64fb7c5189c72de4cb0301e8653ad8
prrrmd <68864853+prrrmd@users.noreply.github.com>
2020-08-19 00:41:53 +0530

    12_middleware

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\worker;
+
+class StudentController extends Controller
+{
+    function index()
+    {
+    	return "Hello";
+    }
+}
---
+++ resources/views/access_denied.blade.php
@@ -0,0 +1,1 @@
+<h2>Accesss Denied :( </h2>
