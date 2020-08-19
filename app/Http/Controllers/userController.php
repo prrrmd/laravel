644322192fdca90610086851692ecfc69de43de6
prrrmd <68864853+prrrmd@users.noreply.github.com>
2020-08-19 23:19:54 +0530

    22_Db_Query_builder_INSERT_UPDATE_DELETE_SELECT_COUNT_FIND

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -9,6 +9,34 @@ class userController extends Controller
 {
     function index()
     {
-    	return DB::select('select * from students');
+    	//return DB::select('select * from students');
+
+    	#Select all records
+    	//return DB::table('students')->get();
+
+    	#Select specific record
+    	// return DB::table('students')
+    	// ->where('name','Akash')
+    	// ->get();
+
+    	#Delete Record
+    	// DB::table('students')
+    	// ->where('name','Shubham')
+    	// ->delete();
+
+    	#Insert Record
+    	// DB::table('students')
+    	// ->insert(['name'=>'Shubham']);
+
+    	#Update Record
+    	DB::table('students')
+    	->where('id',4)
+    	->update(['name'=>'Jayant']);
+
+    	// $data=DB::table('students')->find(4); //this will apply on id automatically
+    	// print_r($data);
+
+    	// $data=DB::table('students')->count();//give total count of records
+    	// echo $data;
     }
 }
